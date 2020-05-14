cd16f8ed595d99e6152a1e13aba88fc51153cb70
llxhz <1481010383@qq.com>
2020-05-14 16:25:57 +0800

    push to master: 新增canvans 画图示例

---
+++ src/pages/canvas/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * title: 'canvas'
+ */
+
+import React from 'react';
+import styles from './index.less'
+
+export default class Index extends  React.Component{
+  private myRef: React.RefObject<HTMLCanvasElement>;
+  constructor(props: any) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  render() {
+    return <canvas className={styles.myCanvas} ref={this.myRef}>对不起，您的浏览器不支持canvas</canvas>;
+  }
+}
