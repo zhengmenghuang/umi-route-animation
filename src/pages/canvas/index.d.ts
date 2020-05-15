315d295cc473a4aab88535921b1f8ddc49b73261
llxhz <1481010383@qq.com>
2020-05-15 15:10:51 +0800

    push to master: 新增canvans 画图示例

---
+++ src/pages/canvas/index.d.ts
@@ -0,0 +1,3 @@
+export interface State {
+  canvansSize: {x: number, y: number}
+}
--- src/pages/canvas/index.tsx
+++ src/pages/canvas/index.tsx
@@ -3,15 +3,83 @@
  */
 
 import React from 'react';
-import styles from './index.less'
+import styles from './index.less';
+import { State } from './index.d'
 
-export default class Index extends  React.Component{
-  myRef: React.RefObject<HTMLCanvasElement>;
+export default class Index extends  React.Component<{}, State>{
+  canvasRef: React.RefObject<HTMLCanvasElement>;
+  boardAreaRef: React.RefObject<HTMLDivElement>;
+  currentPostion: { x: number, y: number };
+  ctx?: CanvasRenderingContext2D | null;
   constructor(props: any) {
     super(props);
-    this.myRef = React.createRef();
+    this.canvasRef = React.createRef();
+    this.boardAreaRef = React.createRef();
+    this.currentPostion = {x: 0, y: 0};
+
+    // 触发页面渲染的东西必须放在这里面
+    this.state = {
+      canvansSize: {x: 0, y: 0}
+    }
   }
+
+  // 生命周期 页面加载完成
+  componentDidMount() {
+    // 设置宽高
+    this.boardAreaRef.current && (this.setState({
+      canvansSize: {
+        x: this.boardAreaRef.current.offsetWidth,
+        y: this.boardAreaRef.current.offsetHeight
+      }
+    }))
+
+    // 获取白板
+    this.canvasRef.current && (this.ctx = this.canvasRef.current.getContext('2d'));
+  }
+
+  // 画笔开始
+  onTouchStart = (e: React.TouchEvent<HTMLCanvasElement>) => {
+    this.currentPostion = {
+      x: e.touches[0].clientX,
+      y: e.touches[0].clientY
+    }
+  }
+
+  // 画笔移动
+  onTouchMove = (e: React.TouchEvent<HTMLCanvasElement>) => {
+    if (!this.ctx) return;
+    const event = e.touches[0];
+    // 设置画笔颜色
+    this.ctx.strokeStyle = '#ff0000';
+    // 设置线条宽度
+    this.ctx.lineWidth = 2;
+    // 开始绘画
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.currentPostion.x, this.currentPostion.y);
+    this.ctx.lineTo(event.clientX, event.clientY);
+    this.ctx.stroke();
+
+    this.currentPostion = {
+      x: event.clientX,
+      y: event.clientY
+    };
+  }
+
+  // 画笔结束
+  onTouchEnd = (e: React.TouchEvent<HTMLCanvasElement>) => {
+
+  }
+
+
   render() {
-    return <canvas className={styles.myCanvas} ref={this.myRef}>对不起，您的浏览器不支持canvas</canvas>;
+    const { canvansSize } = this.state;
+
+    return (
+      <div className={styles.boardArea} ref={this.boardAreaRef}>
+        <canvas width={canvansSize.x} height={canvansSize.y} className={styles.board} ref={this.canvasRef} onTouchStart={this.onTouchStart} onTouchMove={this.onTouchMove} onTouchEnd={this.onTouchEnd}>
+          对不起，您的浏览器不支持canvas
+        </canvas>
+      </div>
+    );
   }
 }
