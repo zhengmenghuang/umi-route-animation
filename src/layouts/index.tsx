49b05f53697fd8ccd56eaeaeccb46873a9750c83
llxhz <1481010383@qq.com>
2020-07-09 11:11:09 +0800

    push to master: null

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import withRouter from 'umi/withRouter';
+// @ts-ignore
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const ANIMATION_MAP: any = {
@@ -9,7 +10,7 @@ const ANIMATION_MAP: any = {
 
 export default withRouter(({location, children, history}: any) => {
   return (
-    <TransitionGroup childFactory={child => React.cloneElement( child, {classNames: ANIMATION_MAP[history.action]})}>
+    <TransitionGroup childFactory={(child: React.FunctionComponentElement<{ classNames: any; }>) => React.cloneElement( child, {classNames: ANIMATION_MAP[history.action]})}>
       <CSSTransition key={location.pathname} timeout={300}>
         {children}
       </CSSTransition>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,14 @@ import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
 import router from 'umi/router';
 
 export default class Index extends  React.Component{
-  goTest() {
-    router.push('/test')
-  }
-
   render() {
     return (
       <WingBlank>
         <WhiteSpace />
-        <Button type="primary" onClick={this.goTest}>test</Button>
+          <Button type="primary" onClick={() => router.push('/test')}>test</Button>
+        <WhiteSpace />
+        <WhiteSpace />
+          <Button type="primary" onClick={() => router.push('/canvas')}>canvas</Button>
         <WhiteSpace />
       </WingBlank>
     );
